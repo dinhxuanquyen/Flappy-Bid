f2f45e7746e6345a36ddc40f57d468b1de896d66
Quyen_Dinh <dinhxuanquyen324@gmail.com>
2024-08-20 22:18:54 +0700

    tạo sàn và cho 2 cái sàn lặp lại liên tục

--- game.py
+++ game.py
@@ -1,9 +1,18 @@
 import pygame, sys
+def draw_floor():
+    screen.blit(floor,(floor_x_pos,600))
+    screen.blit(floor,(floor_x_pos+432,600))
+
+
 pygame.init()
 screen= pygame.display.set_mode((432,768))
 clock = pygame.time.Clock()
 bg = pygame.image.load('assests//background-night.png')
 bg = pygame.transform.scale2x(bg)
+floor = pygame.image.load('assests/floor.png')
+floor = pygame.transform.scale2x(floor)
+floor_x_pos = 0
+
 
 while True:
     for event in pygame.event.get():
@@ -11,5 +20,11 @@ while True:
             pygame.quit()
             sys.exit()
     screen.blit(bg,(0,0))
+    floor_x_pos -= 1
+    draw_floor()
+
+    if floor_x_pos <= -432:
+        floor_x_pos = 0
+
     pygame.display.update()
     clock.tick(120)
