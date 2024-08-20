038d1f47df136d142375daf55cd357fc2c9dae87
Quyen_Dinh <dinhxuanquyen324@gmail.com>
2024-08-20 23:14:53 +0700

    tạo hệ thống ống'

--- game.py
+++ game.py
@@ -1,12 +1,33 @@
-import pygame, sys
+import pygame, sys, random
+#tạo hàm cho trò chơi
 def draw_floor():
-    screen.blit(floor,(floor_x_pos,600))
-    screen.blit(floor,(floor_x_pos+432,600))
+    screen.blit(floor,(floor_x_pos,650))
+    screen.blit(floor,(floor_x_pos+432,650))
+
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop =(500,random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midtop =(500,random_pipe_pos-650))
+    return bottom_pipe, top_pipe
+
+def move_pipe(pipes):
+    for pipe in pipes :
+        pipe.centerx -= 5 
+    return pipes    
+
+def draw_pipe(pipes):
+    for pipe in pipes:
+        if pipe.bottom >= 768 :
+             screen.blit(pipe_surface, pipe)
+        else:
+             flip_pipe = pygame.transform.flip(pipe_surface,False,True)
+             screen.blit(flip_pipe, pipe)
 
 
 pygame.init()
 screen= pygame.display.set_mode((432,768))
 clock = pygame.time.Clock()
+#tạo các biến của trò chơi
 gravity = 0.25
 bird_movement = 0
 # chèn backgroud
@@ -20,6 +41,16 @@ floor_x_pos = 0
 bird = pygame.image.load('assests/yellowbird-midflap.png').convert()
 bird = pygame.transform.scale2x(bird)
 bird_rect = bird.get_rect(center = (100,384))
+#tạo ống
+pipe_surface = pygame.image.load('assests/pipe-green.png').convert()
+pipe_surface = pygame.transform.scale2x(pipe_surface)
+pipe_list =[]
+
+#tạo timer
+spawnpipe=pygame.USEREVENT
+pygame.time.set_timer(spawnpipe, 1200)
+pipe_height = [200,300,400]
+
 
 
 while True:
@@ -31,11 +62,19 @@ while True:
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
                 bird_movement =-11
+        if event.type == spawnpipe:
+             pipe_list.extend(create_pipe())
+
+
     screen.blit(bg,(0,0))
+    #bird
     bird_movement += gravity
     bird_rect.centery += bird_movement
     screen.blit(bird,bird_rect)
-    
+    #ống
+    pipe_list = move_pipe(pipe_list)
+    draw_pipe(pipe_list)
+    # sàn
     floor_x_pos -= 1
     draw_floor()
 
