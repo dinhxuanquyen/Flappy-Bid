2e06ecd9c898e641966ca61eb17c06283e0ca172
Quyen_Dinh <dinhxuanquyen324@gmail.com>
2024-08-21 00:12:39 +0700

    hiệu ứng đập cánh và hệ thống tính điểm

--- game.py
+++ game.py
@@ -31,13 +31,44 @@ def check_collision(pipes):
             return False
     return True
 
+def rotate_bird(bird1):
+    new_bird = pygame.transform.rotozoom(bird1,-bird_movement*3,1)
+    return new_bird
+
+def bird_animation():
+    new_bird = bird_list[bird_index]
+    new_bird_rect = new_bird.get_rect(center = (100,bird_rect.centery))
+    return new_bird, new_bird_rect
+
+def score_display(game_sate):
+    if game_sate == 'main game':
+        score_surface = game_font.render(str(int(score)),True,(255,255,255))
+        score_rect = score_surface.get_rect(center = (216,100))
+        screen.blit(score_surface,score_rect)
+    if game_sate == 'game_over':
+        score_surface = game_font.render(f'Score: {int(score)}',True,(255,255,255))
+        score_rect = score_surface.get_rect(center = (216,100))
+        screen.blit(score_surface,score_rect)
+
+        hight_score_surface = game_font.render(f'High Score: {int(high_score)}',True,(255,255,255))
+        hight_score_rect = hight_score_surface.get_rect(center = (216,610))
+        screen.blit(hight_score_surface,hight_score_rect)
+
+def update_score(score,high_score):
+    if score >  high_score:
+        high_score = score
+    return high_score
+
 pygame.init()
 screen= pygame.display.set_mode((432,768))
 clock = pygame.time.Clock()
+game_font = pygame.font.Font('04B_19.ttf',40)
 #tạo các biến của trò chơi
 gravity = 0.25
 bird_movement = 0
 game_active = True
+score = 0
+high_score = 0
 # chèn backgroud
 bg = pygame.image.load('assests//background-night.png').convert()
 bg = pygame.transform.scale2x(bg)
@@ -46,9 +77,19 @@ floor = pygame.image.load('assests/floor.png').convert()
 floor = pygame.transform.scale2x(floor)
 floor_x_pos = 0
 #tạo bird
-bird = pygame.image.load('assests/yellowbird-midflap.png').convert()
-bird = pygame.transform.scale2x(bird)
+bird_down = pygame.transform.scale2x(pygame.image.load('assests/yellowbird-downflap.png').convert_alpha())
+bird_mid = pygame.transform.scale2x(pygame.image.load('assests/yellowbird-midflap.png').convert_alpha())
+bird_up = pygame.transform.scale2x(pygame.image.load('assests/yellowbird-upflap.png').convert_alpha())
+bird_list = [bird_down,bird_mid,bird_up] # 0 1 2
+bird_index = 0
+bird = bird_list[bird_index]
+
+#bird = pygame.image.load('assests/yellowbird-midflap.png').convert_alpha()
+#bird = pygame.transform.scale2x(bird)
 bird_rect = bird.get_rect(center = (100,384))
+#tạo timmer cho bird
+birdflap = pygame.USEREVENT + 1
+pygame.time.set_timer(birdflap,200)
 #tạo ống
 pipe_surface = pygame.image.load('assests/pipe-green.png').convert()
 pipe_surface = pygame.transform.scale2x(pipe_surface)
@@ -75,20 +116,33 @@ while True:
                 pipe_list.clear()
                 bird_rect.center = (100,384)
                 bird_movement = 0
+                score = 0
         if event.type == spawnpipe:
              pipe_list.extend(create_pipe())
+        if event.type == birdflap:
+            if bird_index < 2:
+                bird_index += 1
+            else:
+                bird_index =0
+            bird, bird_rect = bird_animation()
+  
 
-
-    screen.blit(bg,(0,0))
+    screen.blit(bg,(0,0)) 
     if game_active:
         #bird
         bird_movement += gravity
+        rotated_bird = rotate_bird(bird)
         bird_rect.centery += bird_movement
-        screen.blit(bird,bird_rect)
+        screen.blit(rotated_bird,bird_rect)
         game_active= check_collision(pipe_list)
         #ống
         pipe_list = move_pipe(pipe_list)
         draw_pipe(pipe_list)
+        score += 0.01
+        score_display('main game')
+    else:
+        high_score = update_score(score,high_score)
+        score_display('game_over')
     # sàn
     floor_x_pos -= 1
     draw_floor()
