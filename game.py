a2cf42c76c75085ebd12314c30dac4f243e5814c
Quyen_Dinh <dinhxuanquyen324@gmail.com>
2024-08-20 12:33:42 +0700

    khung của sổ đen

---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame, sys
+pygame.init()
+screen= pygame.display.set_mode((432,768))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
