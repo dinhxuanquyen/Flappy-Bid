d2910dff9467a6c1474ff186ba7ef02e835bbf42
Quyen_Dinh <dinhxuanquyen324@gmail.com>
2024-08-20 23:33:42 +0700

    Xử lý va chạm và kết thúc va chạm

--- game.py
+++ game.py
@@ -17,12 +17,19 @@ def move_pipe(pipes):
 
 def draw_pipe(pipes):
     for pipe in pipes:
-        if pipe.bottom >= 768 :
+        if pipe.bottom >= 600 :
              screen.blit(pipe_surface, pipe)
         else:
              flip_pipe = pygame.transform.flip(pipe_surface,False,True)
              screen.blit(flip_pipe, pipe)
 
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+    if bird_rect.top <= -75 or bird_rect.bottom >= 650:
+            return False
+    return True
 
 pygame.init()
 screen= pygame.display.set_mode((432,768))
@@ -30,6 +37,7 @@ clock = pygame.time.Clock()
 #tạo các biến của trò chơi
 gravity = 0.25
 bird_movement = 0
+game_active = True
 # chèn backgroud
 bg = pygame.image.load('assests//background-night.png').convert()
 bg = pygame.transform.scale2x(bg)
@@ -59,21 +67,28 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
                 bird_movement =-11
+            if event.key == pygame.K_SPACE and game_active==False:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (100,384)
+                bird_movement = 0
         if event.type == spawnpipe:
              pipe_list.extend(create_pipe())
 
 
     screen.blit(bg,(0,0))
-    #bird
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
-    screen.blit(bird,bird_rect)
-    #ống
-    pipe_list = move_pipe(pipe_list)
-    draw_pipe(pipe_list)
+    if game_active:
+        #bird
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        screen.blit(bird,bird_rect)
+        game_active= check_collision(pipe_list)
+        #ống
+        pipe_list = move_pipe(pipe_list)
+        draw_pipe(pipe_list)
     # sàn
     floor_x_pos -= 1
     draw_floor()
